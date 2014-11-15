2c551d2df49e28785c436267a873cf49e48d327f
Andrew Wilder <andrew.m.wilder@gmail.com>
2014-11-15 18:44:00 -0500

    Skeletons complete

--- src/SDDRServer.java
+++ src/SDDRServer.java
@@ -4,7 +4,6 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.regex.Pattern;
 
 import javax.net.ServerSocketFactory;
 import javax.net.ssl.SSLServerSocketFactory;
@@ -15,24 +14,95 @@ import javax.net.ssl.SSLServerSocketFactory;
  */
 public class SDDRServer extends Thread {
 	
+	/** Port that SDDR server always binds to */
+	private static final int SDDR_PORT = 40231;
+	
+	/**
+	 * put(Document UID, SecurityFlag): A document is sent to the server over the secure channel that was 
+	 * established when the session was initiated. If the document already exists on the server, you may 
+	 * overwrite it along with its meta-data. If a new document is put, this client becomes the owner of the 
+	 * document. If the client is able to update because of a delegation, the owner does not change. You need to 
+	 * use some scheme to ensure that documents created by different clients have unique UIDs. The 
+	 * SecurityFlag specifies how document data should be stored on the server
+	 */
+	private void put() {
+		System.out.println("Received command from " + clientname + ": put");
+		//TODO
+	}
+	
+	/**
+	 * get(Document UID): After a session is established, a client can request a document over the secure 
+	 * channel from the server using this call. A request coming over a session is honored only if it is for a 
+	 * document owned by the client that set up the session, or this client has a valid delegation (see delegate 
+	 * call). If successful, a local copy of the document data is made available to the client. The server must 
+	 * maintain information about documents (e.g., meta-data) that allows it to locate the requested document, 
+	 * decrypt it and send the data to the requestor
+	 */
+	private void get() {
+		System.out.println("Received command from " + clientname + ": get");
+		//TODO
+	}
+	
+	/**
+	 * delegate(Document UID, client C, time T, PropagationFlag): A delegation credential (e.g., signed 
+	 * token) is generated that allows an owner client to delegate rights (put, get or both) for a document to 
+	 * another client C for a time duration of T. If C is ALL, the delegation is made to any client in the system 
+	 * for this document. If you use a protocol that needs to securely exchange messages between clients for 
+	 * implementing delegation, you should use secure channels. PropagationFlag is a Boolean that specifies if 
+	 * C can propagate the rights delegated to it. A true value permits delegation and false disallows it
+	 */
+	private void delegate() {
+		System.out.println("Received command from " + clientname + ": delegate");
+		//TODO
+	}
+	
 	/**
 	 * Main server method to run after arguments are validated.
 	 * Bind to a port for initiating secure sessions with clients.
 	 */
-	private Socket socket;
+	private Socket socket = null;
+	private String clientname = null;
+	private BufferedReader in = null;
+	private PrintWriter out = null;
 	public void run() {
-		System.out.println("Connected to user " + socket.getInetAddress().toString().substring(1) +
-				":" + socket.getPort() + "!");
+		System.out.println("Connected to user " + clientname + ":" + socket.getPort() + "!");
+		
+		// Fields used to handle the client connection
+		boolean finished = false;
+		
 		try {
 			// Get input and output handles for client communication
-			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			PrintWriter writer = new PrintWriter(socket.getOutputStream());
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new PrintWriter(socket.getOutputStream());
 			
-			// Initiate secure session with client
-			
+			// Receive commands from client until user ends the session
+			do {
+				String command = in.readLine();
+				
+				switch(command) {
+				case "end-ssession":
+					finished = true;
+					break;
+				case "put":
+					put();
+					break;
+				case "get":
+					get();
+					break;
+				case "delegate":
+					delegate();
+					break;
+				default:
+					System.out.println("Unknown command received: " + command);
+				}
+			} while(!finished);
+			in.close();
+			out.close();
 		} catch(IOException e) {
-			sddrserver_fail("Server encountered an error: " + e.getMessage());
+			System.out.println("Connection to user " + clientname + " interrupted: " + e.getMessage());
 		}
+		System.out.println("Ended connection with user " + socket.getInetAddress().toString().substring(1) +
+				":" + socket.getPort() + ".");
 	}
 	
 	/**
@@ -41,7 +111,7 @@ public class SDDRServer extends Thread {
 	 */
 	public SDDRServer(Socket socket) {
 		this.socket = socket;
-		System.out.println("Accepted connection from " + socket.getInetAddress() + ":" + socket.getPort());
+		clientname = socket.getInetAddress().toString().substring(1);
 	}
 	
 	/**
@@ -58,20 +128,14 @@ public class SDDRServer extends Thread {
 	 * @param args The port to bind this server to
 	 */
 	public static void main(String[] args) {
-		if(args.length != 1) {
-			sddrserver_fail("Program usage: java SDDRServer <port>");
-		}
-		if(!Pattern.matches("\\d+", args[0])) {
-			sddrserver_fail("Invalid port: " + args[0]);
-		}
-		if(Integer.parseInt(args[0]) >= 1 << 16) {
-			sddrserver_fail("Port out of range: " + args[0]);
+		if(args.length != 0) {
+			sddrserver_fail("Program usage: java SDDRServer");
 		}
 		ServerSocketFactory ssf = SSLServerSocketFactory.getDefault();
 		ServerSocket ss = null;
 		try {
-			ss = ssf.createServerSocket(Integer.parseInt(args[0]));
-			System.out.println("Listening for incoming connections on port " + args[0] + "...");
+			ss = ssf.createServerSocket(SDDR_PORT);
+			System.out.println("Listening for incoming connections on port " + SDDR_PORT + "...");
 			while(true) {
 				new SDDRServer(ss.accept()).start();
 			}
